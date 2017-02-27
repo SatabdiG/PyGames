6edeb1ae141eb333a8e8c8bacb57e9dc597344f3
SatabdiG <satabdi.ganguly89@gmail.com>
2017-02-27 12:40:11 +0100

    Loop screen

--- Basic1-Shapes.py
+++ Basic1-Shapes.py
@@ -17,11 +17,33 @@ screen=pygame.display.set_mode((width, height))
 screen.fill(black)
 pygame.display.set_caption("First Game", "First Game")
 
+clock=pygame.time.Clock()
+FPS = 24
+
+clr1=(22, 122, 211)
+clr2=(100,10,133)
+clr3=(54, 67,78)
+
 ### Main Loop
 
 while 1:
+    #PROCESS
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.quit()
             sys.exit()
 
+
+    #PROCESS
+    #LOGIC
+
+    #LOGIC
+    #DRAW
+    pygame.draw.line(screen, clr1, (0 , 0), (width, height))
+    pygame.draw.rect(screen, clr3, (40,40, 300, 100))
+
+    pygame.draw.circle(screen, clr2, (350,200),80,2)
     pygame.display.flip()
+    #DRAW
+
+    clock.tick(FPS)
